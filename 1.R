755cba2354f15c2379af1215c6b0286b4be8887b
moradkasmi <moradkasmi061@gmail.com>
2022-08-24 17:35:07 +0200

    New

--- 1.R
+++ 1.R
@@ -1,3 +1,10 @@
 getwd()
 1+1
 print("hahahaha")
+library(tidyverse)
+library(ggplot2)
+ggplot2(mtcars,
+        mapping = aes(x = mpg, y = cyl)+
+          geom_point(na.rm = T)
+        
+        
